7650114395cf9a638c8b74a2e77cae179a320117
Enea <enealila@outlook.com>
2018-04-10 16:57:43 +0200

    add Get /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo}= require('./models/todo');
 var {User}= require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -99,6 +100,13 @@ app.post('/users',(req,res)=>{
 		res.status(400).send(e);
 	});
 });
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+
+	res.send(req.user);
+	});
 
 
 app.listen(3000,()=>{
